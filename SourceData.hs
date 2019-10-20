bf901568eb648ae227b54d87ad5569d8803731ee
rlnsy <rowlindsay@gmail.com>
2019-10-20 14:02:14 -0700

    working meta parse

--- SourceData.hs
+++ SourceData.hs
@@ -1,11 +1,13 @@
-{-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
+{-# LANGUAGE FlexibleInstances, OverloadedStrings#-}
 
 module SourceData where
 
     import Data.Aeson
+    import Data.Aeson.Types
     import System.IO
     import qualified Data.ByteString.Lazy as B
     import qualified Data.ByteString.Lazy.Char8 as C
+    import Data.HashMap.Strict
 
     -- These define the elementary data schemas as they appear
     -- in the JSON files. Relations (such as links, parentNode, meta) 
@@ -14,27 +16,31 @@ module SourceData where
     data StoryPort = 
         -- Represents a choice at a given scene. links is a list of 
         -- ids of the dst scenes.
-        StoryPort { port_id             :: String
-                ,   port_type           :: String
-                ,   port_name           :: String
-                ,   parentNode          :: String
-                ,   links               :: [String]
-                ,   port_in             :: Bool
-                ,   label               :: String
+        StoryPort {     port_id             :: String
+                ,       port_type           :: String
+                ,       port_name           :: String
+                ,       parentNode          :: String
+                ,       links               :: [String]
+                ,       port_in             :: Bool
+                ,       label               :: String
         } deriving (Show)
 
     data StoryScene =
          -- Represents a scene with a list of ports. scene_name is
          -- the primary text which appears above the scene page in the
          -- web view. Full body text is contained in the meta
-        StoryScene { scene_id           :: String
-                ,   scene_type          :: String
-                ,   scene_ports         :: [StoryPort]
-                ,   scene_name          :: String
+        StoryScene {    scene_id            :: String
+                ,       scene_type          :: String
+                ,       scene_ports         :: [StoryPort]
+                ,       scene_name          :: String
         } deriving (Show)
 
-    -- data MetaEntry =
-    --     -- Additional scene information, such as text
+    data SceneMeta =
+        -- Additional scene information, such as text
+        -- link_id is the corresponding scene's id
+        SceneMeta {     link_id             :: String
+                ,       text                :: String
+        } deriving (Show)
 
     -- object translation from JSON. keys refer to the original keys in 
     -- the file.
@@ -57,6 +63,14 @@ module SourceData where
                         <*> o .: "ports"
                         <*> o .: "name"
 
+    decodeMetaEntry :: (String, Value) -> Parser SceneMeta
+    decodeMetaEntry (key, val) = withObject "meta info" (\ o ->
+            SceneMeta key <$> o .: "text") val
+
+    instance {-# OVERLAPS #-} FromJSON [SceneMeta] where
+        parseJSON x =
+            parseJSON x >>= mapM decodeMetaEntry . toList
+
 
     -- Decode test code
 
@@ -66,14 +80,30 @@ module SourceData where
     getJSON :: IO B.ByteString
     getJSON = B.readFile testJSON
 
+    testJSONMeta :: FilePath
+    testJSONMeta = "example_data/test_meta.json"
+
+    getJSONMeta :: IO B.ByteString
+    getJSONMeta = B.readFile testJSONMeta
+
     test :: IO ()
     test = do
-        putStrLn "%================ Storyboard Parse Demo =================%"
+        putStrLn "%================ Storyboard Parse Test =================%"
         putStrLn "(This should parse a single scene from file)"
         putStrLn "Decoding JSON from file..."
         content <- getJSON
         C.putStrLn content
         d <- (eitherDecode <$> getJSON) :: IO (Either String StoryScene)
+        Prelude.putStrLn "Result:"
+        case d of
+            Left err -> putStrLn err
+            Right ps -> print ps
+        putStrLn "%--------------------------------------------------------%"
+        putStrLn "(This should parse a list of scene metas from file)"
+        putStrLn "Decoding..."
+        content <- getJSONMeta
+        C.putStrLn content
+        d <- (eitherDecode <$> getJSONMeta) :: IO (Either String [SceneMeta])
         Prelude.putStrLn "Result:"
         case d of
             Left err -> putStrLn err
